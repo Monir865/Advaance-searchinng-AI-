f3e079f86ab85d71840152aafa033394b57f60a3
Monir <monir.khan.city@gmail.com>
2023-04-06 18:04:03 +0600

    add new function

---
+++ main.js
@@ -0,0 +1,13 @@
+var hlist = window.history.length; //length of history
+var hisArr = [];    // empty array store prev history
+
+    for ( i = 0; i <= hlist; i++ ) {
+         hisArr.push( url );  
+    };
+
+
+    array.forEach(e => {
+        console.log(e+', ');
+    });
+
+    
